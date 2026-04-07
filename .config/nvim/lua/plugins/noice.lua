19af3d31ee476718d770268a6cf99558eb8aca9b
Mateusz Kurzyna <lolc44t@gmail.com>
2026-04-07 23:36:40 +0200

    nvim: modify noice tooltip

---
+++ .config/nvim/lua/plugins/noice.lua
@@ -0,0 +1,16 @@
+return {
+	"folke/noice.nvim",
+	opts = {
+		views = {
+			hover = {
+				border = {
+					style = "rounded",
+					padding = { 0, 1 },
+				},
+				win_options = {
+					winhighlight = "Normal:HoverNormal,FloatBorder:HoverFloatBorder,NormalFloat:HoverNormal",
+				},
+			},
+		},
+	},
+}
